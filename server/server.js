54b0bb090a5f758842be454151a44aafcc74ec1e
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2020-12-21 13:38:33 -0500

    Bootstrapped client using CRA.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Hello, GSSoc");
+});
 app.listen(3100, () => {
   console.log("APL Server Started in port 3100!");
+});
+app.listen(3200, () => {
+  console.log("APL Server Started in port 3200 also!");
 });
